a415201743d0d6e4dd0fbeeb845f9e675146e11c
Jonas Cooper <Jonascooper19@gmail.com>
2021-02-15 14:09:40 -0500

    Code to Print even numbers from 0 to 100

---
+++ src/week2_lec1_demo1/Week2_Lec1_Demo1.java
@@ -0,0 +1,29 @@
+
+package week2_lec1_demo1;
+
+
+
+
+public class Week2_Lec1_Demo1 { // Camal Case naming
+
+    
+    public static void main(String[] args) {
+        
+       int num = 102;
+       
+               System.out.print("List of even numbers from 0 to "+num+":\n ");
+               
+               for (int i = 0; i<num;i++){ //checks if number is even
+                   if (i%2 ==0){ // if the number is divisble by 2, the numberis even
+                       
+                   
+               
+                   System.out.println (i); // prints out results
+                   
+                   //System.out.println(num.nextLine());
+                   }
+               }
+    }
+}
+        
+        
